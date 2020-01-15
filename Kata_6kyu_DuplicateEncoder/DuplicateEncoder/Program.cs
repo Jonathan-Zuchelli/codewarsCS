dd919f1cf74c748ef1a6b263cb39687112e4c3e8
Jonathan Zuchelli <jonathan@clubprophetsystems.com>
2020-01-15 15:55:56 -0500

    DuplicateEncode Solution

---
+++ Kata_6kyu_DuplicateEncoder/DuplicateEncoder/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static string DuplicateEncode(string word)
+    {
+        return String.Concat((word.ToLower()).Select(x => word.Where(y => x == y).Count() > 1 ? ')' : '('));
+    }
+}
