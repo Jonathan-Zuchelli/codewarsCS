161ed724d03c7a82304e18ac88f9f10d8dc081d5
Jonathan Zuchelli <jonathan@clubprophetsystems.com>
2020-01-15 16:18:45 -0500

    Logic Fix

--- Kata_6kyu_DuplicateEncoder/DuplicateEncoder/Program.cs
+++ Kata_6kyu_DuplicateEncoder/DuplicateEncoder/Program.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿//The goal of this exercise is to convert a string to a new string where 
+//    each character in the new string is "(" if that character appears 
+//    only once in the original string, or ")" if that character appears 
+//    more than once in the original string. Ignore capitalization when 
+//    determining if a character is a duplicate.
+
+using System;
 using System.Linq;
 
 public class Kata
 {
     public static string DuplicateEncode(string word)
     {
-        return String.Concat((word.ToLower()).Select(x => word.Where(y => x == y).Count() > 1 ? ')' : '('));
+        word = word.ToLower();
+        
+        return String.Concat(word.Select(x => word.Where(y => x == y).Count() > 1 ? ')' : '('));
     }
 }
