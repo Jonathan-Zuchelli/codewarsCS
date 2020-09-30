5527dc048c5dd10b292199454ef6d0e5462b94e8
Jonathan Zuchelli <jrzuchelli@gmail.com>
2020-09-30 13:45:55 -0400

    Initial Commit
    Initial commit

---
+++ MaximumSubarraySum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MaximumSubarraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Answer: " + MaxSeq(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+        }
+
+        public static int MaxSeq(int[] arr){
+            int bestSum = 0;
+            int currentSum = 0;
+
+            for (int i = 0; i < arr.Length; i++){
+                currentSum = Math.Max(0, currentSum + arr[i]);
+                bestSum = Math.Max(bestSum, currentSum);
+            }
+            return bestSum;
+        }
+    }    
+}
