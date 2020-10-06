468b03ad37230ffa63a7466ad4f54f63b83e2707
Jonathan Zuchelli <jrzuchelli@gmail.com>
2020-10-06 16:15:49 -0400

    Inital commit
    Inital Commit

---
+++ HighestAndLowest/HighestAndLowest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HighestAndLowest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(HighAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
+            Console.ReadLine();
+        }
+
+        public static string HighAndLow(string numbers)
+        {
+            String[] newNumbers = numbers.Split(" ");
+            int min = Int32.Parse(newNumbers[0]);
+            int max = Int32.Parse(newNumbers[0]);
+            foreach (String number in newNumbers)
+            {
+                min = Math.Min(min, Int32.Parse(number));
+                max = Math.Max(max, Int32.Parse(number));
+            }
+            return max + " " + min;
+        }
+    }
+}
