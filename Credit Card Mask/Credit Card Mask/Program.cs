ba605b7fb27a59f3efb7dbb962e030866be667a4
Jonathan Zuchelli <jrzuchelli@gmail.com>
2020-10-13 17:47:21 -0400

    Initial Commit
    Initial Commit

---
+++ Credit Card Mask/Credit Card Mask/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Credit_Card_Mask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Maskify("111111111"));
+            Console.ReadLine();
+        }
+        public static string Maskify(string cc)
+        {
+            if (cc.Length < 4)
+            {
+                return cc;
+            }
+            return (String.Concat(Enumerable.Repeat("#", cc.Length - 4))) + cc.Substring(cc.Length - 4);
+        }
+    }
+}
